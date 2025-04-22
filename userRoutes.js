2a03930839742df3729106a6c8815276f13703cd
PRISCILAGARCIA18 <157067219+PRISCILAGARCIA18@users.noreply.github.com>
2025-04-21 19:12:50 -0600

    Delete userRoutes.js

--- userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { crearUsuario, obtenerUsuarios } = require("../controllers/userController");
-
-router.post("/api/usuarios", crearUsuario);
-router.get("/api/usuarios", obtenerUsuarios);
-
-module.exports = router;
