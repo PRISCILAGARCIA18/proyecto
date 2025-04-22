60051f01ff9a2d6c86c999d100dba52acf6fb849
PRISCILAGARCIA18 <157067219+PRISCILAGARCIA18@users.noreply.github.com>
2025-04-21 19:12:30 -0600

    Delete user.js

--- user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  rol: { type: String, required: true }
-});
-
-module.exports = mongoose.model("User", userSchema);
